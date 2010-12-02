f174215024ee3a6651b4331443ccd08993bad23f
jd <jan.depke@haw-hamburg.de>
2010-12-02 15:18:04 +0100

    initial java sources

---
+++ java/Rules/IRuleEngine.java
@@ -0,0 +1,17 @@
+package Rules;
+import java.util.*;
+
+/**
+ * @author jdepke
+ * @version 1.0
+ * @created 02-Dez-2010 14:47:40
+ */
+public interface IRuleEngine {
+
+	/**
+	 * 
+	 * @param list
+	 */
+	public ArrayList<Integer> calculate(List<Integer> list);
+
+}
---
+++ java/Rules/RuleEngine.java
@@ -0,0 +1,27 @@
+package Rules;
+import java.util.*;
+
+/**
+ * @author jdepke
+ * @version 1.0
+ * @created 02-Dez-2010 14:47:41
+ */
+public class RuleEngine implements IRuleEngine {
+
+	public RuleEngine(){
+
+	}
+
+	public void finalize() throws Throwable {
+
+	}
+
+	/**
+	 * 
+	 * @param list
+	 */
+	public ArrayList<Integer> calculate(List<Integer> list){
+		return null;
+	}
+
+}
