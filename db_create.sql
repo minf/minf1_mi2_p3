c24707e4750dd0ab41e61da77be03401723fe8aa
jd <jan.depke@haw-hamburg.de>
2010-12-02 14:45:22 +0100

    SQL create cmds

---
+++ db_create.sql
@@ -0,0 +1,25 @@
+CREATE  TABLE IF NOT EXISTS `category` (
+  `id` INT NOT NULL ,
+  `parent_id` INT NULL ,
+  `name` VARCHAR(200) NULL ,
+  `description` VARCHAR(200) NULL ,
+  PRIMARY KEY (`id`) )
+ENGINE = MyISAM
+DEFAULT CHARACTER SET = utf8;
+
+
+CREATE  TABLE IF NOT EXISTS `option` (
+  `id` INT NOT NULL ,
+  `category_id` INT NULL ,
+  `name` VARCHAR(200) NULL ,
+  `description` VARCHAR(200) NULL ,
+  `img` VARCHAR(200) NULL ,
+  PRIMARY KEY (`id`) ,
+  INDEX `category_id` (`category_id` ASC) ,
+  CONSTRAINT `category_id`
+    FOREIGN KEY (`category_id` )
+    REFERENCES `category` (`id` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = MyISAM
+DEFAULT CHARACTER SET = utf8;
