ac460719a8a817d09c12c54077746b94472a7212
erik <erik@vontaene.de>
2010-12-02 18:36:42 +0100

    Initial java socket tests

--- java/main.java
+++ java/main.java
@@ -1,6 +1,14 @@
+import Net.*;
+
 class main {
 
-	public static void main(String[] args) {
-		System.out.println("test");
+	public static void main(String[] args) throws Exception {
+		Net.server p = new Net.server();
+
+		System.out.println("Starting");
+
+		while(true) {
+			p.receive();
+		}
 	}
 }
