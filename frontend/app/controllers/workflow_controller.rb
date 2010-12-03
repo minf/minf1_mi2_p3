93afd252b28b8094354de21ecb123c082f7c74f4
mr.kamel <mr.kamel@gmx.net>
2010-12-03 13:22:28 +0100

    added options and added ability to skip steps

--- frontend/app/controllers/workflow_controller.rb
+++ frontend/app/controllers/workflow_controller.rb
@@ -9,24 +9,33 @@ class WorkflowController < ApplicationController
 
   def reset
     session[:chosen_options] = []
+    session[:skipped_steps] = []
 
     redirect_to workflow_url
   end
 
   def index
+    # redirect to finish if no more steps
+
+    return redirect_to finish_url unless current_step
+
     available = RuleEngine.calculate chosen_options
 
     # fetch all categories for current step
 
     @category = Category.find_by_workflow_id current_step
 
-    # redirect to finish if no more options available
+    # skip step if category not found
 
-    return redirect_to finish_url unless @category
+    return skip_step unless @category
 
     # options matching available and category
 
     @options = @category.options.find_all_by_article_id available.collect(&:article_id)
+
+    # skip step and try again if no options available
+
+    return skip_step if @options.blank?
   end
 
   def choose_option
@@ -54,6 +63,7 @@ class WorkflowController < ApplicationController
     option = Option.find params[:id]
 
     session[:chosen_options].delete option.id
+    session[:skipped_steps] = []
 
     redirect_to workflow_url
   end
@@ -75,7 +85,7 @@ class WorkflowController < ApplicationController
   # calculate current stop from already chosen options and workflow
 
   def current_step
-    return (workflow - chosen_categories.collect(&:workflow_id)).first
+    return (workflow - chosen_categories.collect(&:workflow_id) - session[:skipped_steps].to_a).first
   end
 
   # calculate current sum for chosen options
@@ -88,5 +98,14 @@ class WorkflowController < ApplicationController
 
   def workflow
     return WORKFLOW.collect(&:values).flatten
+  end
+
+  # skip current step and try again
+
+  def skip_step
+    session[:skipped_steps] ||= []
+    session[:skipped_steps].push current_step
+
+    return redirect_to workflow_url
   end
 end
