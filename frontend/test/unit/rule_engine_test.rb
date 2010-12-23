e8381c7da9edcb1a384ef5bcaeb3cffc817eafa8
mr.kamel <mr.kamel@gmx.net>
2010-12-23 19:35:52 +0100

    tex-protocol includes frontend stuff

--- frontend/test/unit/rule_engine_test.rb
+++ frontend/test/unit/rule_engine_test.rb
@@ -2,6 +2,14 @@ require 'test_helper'
 
 class RuleEngineTest < ActiveSupport::TestCase
   def test_calculate_all
-    assert_equal Option.all, RuleEngine.calculate
+    # can't run assert_equal, because we don't know what the rule engine writes back to us
+
+    assert RuleEngine.calculate
+  end
+
+  # if we ask for compatible options for a specific option, the result set may not include the option
+
+  def test_calculate_without
+    assert ! RuleEngine.calculate(Array Option.first).include?(Option.first)
   end
 end
